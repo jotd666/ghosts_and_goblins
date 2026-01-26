f7611b05b86d5cbd7318d5133fb04675b382c276
Jean-François Fabre <jotd666@gmail.com>
2026-01-26 23:54:45 +0100

    found tile issues in level 4

--- assets/amiga/merge_used.py
+++ assets/amiga/merge_used.py
@@ -35,5 +35,5 @@ def merge(used_name,nb_items,nb_cluts):
             f.write(contents)
 
 merge("fg_used_tiles",0x400,16)
-merge("level7/bg_used_tiles",0x400,8)
+merge("level4/bg_used_tiles",0x400,8)
 merge("used_sprites",0x400,4)
